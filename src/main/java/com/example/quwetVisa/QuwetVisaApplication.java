8b58f290fb52656dd165fceb2f19c4e26d589a9b
nawsish <nawsish.ahmed@spectrum-bd.com>
2024-12-08 11:51:55 +0600

    adding

---
+++ src/main/java/com/example/quwetVisa/QuwetVisaApplication.java
@@ -0,0 +1,13 @@
+package com.example.quwetVisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuwetVisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuwetVisaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/quwetVisa/QuwetVisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quwetVisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuwetVisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
